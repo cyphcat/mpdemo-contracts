07492f74cf149156d0712a7ff9708aea631b876c
cyphcat <98901624+cyphcat@users.noreply.github.com>
2022-05-06 04:31:57 +0800

    export artifacts after deploy

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,33 +1,63 @@
 import {ethers, network} from "hardhat";
+import * as fs from "fs";
+import * as path from "path";
 
 async function main() {
+  const chainId = network.config.chainId || 31337;
 
   const TheCoin = await ethers.getContractFactory("TheCoin");
   const coin = await TheCoin.deploy();
   await coin.deployed();
-  console.log("TheCoin", coin.address);
 
   const TheNFT = await ethers.getContractFactory("TheNFT");
   const nft = await TheNFT.deploy();
   await nft.deployed();
-  console.log("TheNFT", nft.address);
-
-  const StaticMarket = await ethers.getContractFactory("StaticMarket");
-  const staticMarket = await StaticMarket.deploy();
-  await staticMarket.deployed();
-  console.log("StaticMarket", staticMarket.address);
 
   const TheMarketplaceRegistry = await ethers.getContractFactory("TheMarketplaceRegistry");
   const registry = await TheMarketplaceRegistry.deploy();
   await registry.deployed();
-  console.log("TheMarketplaceRegistry", registry.address);
 
   const TheMarketplace = await ethers.getContractFactory("TheMarketplace");
-  const marketplace = await TheMarketplace.deploy(network.config.chainId!, registry.address);
+  const marketplace = await TheMarketplace.deploy(chainId, registry.address);
   await marketplace.deployed();
-  console.log("TheMarketplace", marketplace.address);
+
+  const StaticMarket = await ethers.getContractFactory("StaticMarket");
+  const staticMarket = await StaticMarket.deploy();
+  await staticMarket.deployed();
 
   await registry.grantInitialAuthentication(marketplace.address);
+
+  const addresses = {
+    "TheCoin": coin.address,
+    "TheNFT": nft.address,
+    "TheMarketplaceRegistry": registry.address,
+    "TheMarketplace": marketplace.address,
+    "StaticMarket": staticMarket.address,
+  };
+  console.log(addresses);
+
+  const artifactsDir = path.resolve(__dirname, "../artifacts");
+  const exportDir = path.resolve(process.env.FRONTEND_PROJECT_DIR!, "src/contracts");
+  if (fs.existsSync(exportDir)) {
+    fs.writeFileSync(
+      path.resolve(exportDir, "deployments", chainId + ".json"),
+      JSON.stringify(addresses), "utf-8");
+    fs.copyFileSync(
+      path.resolve(artifactsDir, "contracts/TheCoin.sol/TheCoin.json"),
+      path.resolve(exportDir, "artifacts/TheCoin.json"));
+    fs.copyFileSync(
+      path.resolve(artifactsDir, "contracts/TheNFT.sol/TheNFT.json"),
+      path.resolve(exportDir, "artifacts/TheNFT.json"));
+    fs.copyFileSync(
+      path.resolve(artifactsDir, "contracts/TheMarketplaceRegistry.sol/TheMarketplaceRegistry.json"),
+      path.resolve(exportDir, "artifacts/TheMarketplaceRegistry.json"));
+    fs.copyFileSync(
+      path.resolve(artifactsDir, "contracts/TheMarketplace.sol/TheMarketplace.json"),
+      path.resolve(exportDir, "artifacts/TheMarketplace.json"));
+    fs.copyFileSync(
+      path.resolve(artifactsDir, "wyvern-v3/contracts/StaticMarket.sol/StaticMarket.json"),
+      path.resolve(exportDir, "artifacts/StaticMarket.json"));
+  }
 }
 
 main().catch(e => {
